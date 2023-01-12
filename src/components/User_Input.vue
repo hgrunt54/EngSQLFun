465f525f0cbb5dec73436ec055edaf90a74f63f8
Gary Bouck <hgrunt54@gmail.com>
2023-01-11 23:15:47 -0600

    Updated Form Post

--- src/components/User_Input.vue
+++ src/components/User_Input.vue
@@ -1,12 +1,14 @@
 <template>
     <section>
-        <form>
-            <label>Name: </label>
-            <input type="text" /><br />
-            <label>Email: </label>
-            <input type="text" /><br /><br />
-            <label>Script:</label><br />
-            <textarea></textarea><br />
+        <form id="input-form" class="form" action="https://formsubmit.co/gary.bouck@wolterskluwer.com.com" method="POST">
+            <input type="hidden" name="_subject" value="WK SQL Fun!">
+            <input type="hidden" name="_next" value="https://transcendent-lollipop-48951b.netlify.app/">
+            <label for="empName">Name: </label>
+            <input type="text" id="empName" name="empName" placeholder="Your Name Here" required /><br />
+            <label for="empEmail">Email: </label>
+            <input type="text" id="empEmail" name="empEmail" placeholder="Email Address" required /><br /><br />
+            <h3>Script:</h3>
+            <textarea id="script" name="script" required></textarea><br />
             <input type="submit" value="Submit!" />
         </form>
     </section>
@@ -28,10 +30,11 @@
         resize: none;
     }
     input[type=text] {
+        width: 15vw;
         margin: .5rem;
     }
     input[type=submit] {
-        margin-top: .5rem;
+        margin-top: 1rem;
         padding: .5rem 1rem .5rem 1rem;
     }
 </style>
