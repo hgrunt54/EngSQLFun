644d1584bcde9d455c00014878c64e729758c6e2
Gary Bouck <hgrunt54@gmail.com>
2023-01-11 23:30:38 -0600

    Updated Form Post

--- src/components/User_Input.vue
+++ src/components/User_Input.vue
@@ -1,12 +1,12 @@
 <template>
     <section>
-        <form id="input-form" class="form" action="https://formsubmit.co/gary.bouck@wolterskluwer.com" method="POST">
+        <form id="input-form" class="form" action="https://formsubmit.co/3fb13f7c8487de7f4a7b25ac78add79b" method="POST">
             <input type="hidden" name="_subject" value="WK SQL Fun!">
             <input type="hidden" name="_next" value="https://transcendent-lollipop-48951b.netlify.app/">
             <label for="empName">Name: </label>
-            <input type="text" id="empName" name="empName" placeholder="Your Name Here" required /><br />
+            <input type="text" id="empName" name="Name" placeholder="Your Name Here" required /><br />
             <label for="empEmail">Email: </label>
-            <input type="text" id="empEmail" name="empEmail" placeholder="Email Address" required /><br /><br />
+            <input type="text" id="empEmail" name="Email" placeholder="Email Address" required /><br /><br />
             <h3>Script:</h3>
             <textarea id="script" name="script" required></textarea><br />
             <input type="submit" value="Submit!" />
